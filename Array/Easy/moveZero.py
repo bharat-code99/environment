53556baea5c440e278c3d2d77ecb1c5168a38412
bharat-code99 <2491bhr@gmail.com>
2026-08-25 23:23:57 +0530

    fixed move zeroes

--- Array/Easy/moveZero.py
+++ Array/Easy/moveZero.py
@@ -1,20 +1,16 @@
 def move_zeroes(arr):
-    left = 0
-    right = len(arr)-1
+    i = 0
+    for j in range(len(arr)):
+        if arr[j] != 0:
+            arr[i] = arr[j]
+            i += 1
 
-    while left < right:
-        while left < right and arr[left] != 0:
-            left += 1
-
-        while left < right and arr[right] == 0:
-            right -= 1
-
-        if left < right:
-            arr[left], arr[right] = arr[right], arr[left]
-            left += 1
-            right -= 1
+    while i < len(arr):
+        arr[i] = 0
+        i += 1
 
 
+# [1, 2, 3, 4, 1, 0, 0, 0]
 arr = [1, 0, 2, 3, 0, 4, 0, 1]
 move_zeroes(arr)
 print("Array after moving zeroes: ", arr)
